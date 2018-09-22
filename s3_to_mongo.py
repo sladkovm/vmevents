95b69f383e425c9616ac1685368a61e25430d365
sladkovm <sladkovm@gmail.com>
2018-09-22 21:59:37 +0200

    repopulated db

--- s3_to_mongo.py
+++ s3_to_mongo.py
@@ -1,54 +1,32 @@
-import boto3
+from uboto3 import UBoto3
+from pymongo import MongoClient
 import os
 import json
 import pprint
 
 
-class UBoto3():
-
-    region_name = os.getenv('DO_SPACES_REGION')
-    endpoint_url = 'https://{}.digitaloceanspaces.com'.format(os.getenv('DO_SPACES_REGION'))
-    aws_access_key_id = os.getenv('DO_SPACES_KEY')
-    aws_secret_access_key = os.getenv('DO_SPACES_SECRET')
-    bucket = os.getenv('DO_BUCKET')
-
-    def __init__(self):
-
-        self.client = boto3.client('s3',
-                              region_name=self.region_name,
-                              endpoint_url=self.endpoint_url,
-                              aws_access_key_id=self.aws_access_key_id,
-                              aws_secret_access_key=self.aws_secret_access_key)
-
-    def list_objects(self, **kwargs):
-        """List of object found in the bucket
-
-        Parameters
-        ----------
-        Prefix: str
-            Prefix indicating the subfolder (slash is omited)
-        """
-        r = self.client.list_objects(Bucket=self.bucket, **kwargs)
-        return r.get('Contents')
-
-    def get_object(self, Key, **kwargs):
-        """Get a single object from the bucket using Key"""
-
-        r = self.client.get_object(Bucket=self.bucket, Key=Key, **kwargs)
-
-        return json.loads(r['Body'].read().decode('utf-8'))
-
-
 if __name__ == "__main__":
 
+    uri = "mongodb://{}:{}@{}".format(
+        os.getenv("ME_CONFIG_MONGODB_ADMINUSERNAME"),
+        os.getenv("ME_CONFIG_MONGODB_ADMINPASSWORD"),
+        os.getenv("MONGODB_URL"))
+
+    mongo = MongoClient(uri)
+    db = mongo.velometria
+    collection = db.events
 
     s3 = UBoto3()
-    keys = s3.list_objects(Prefix='events')
 
-    pprint.pprint(keys)
+    keys = s3.list_keys(Prefix='events')
 
-    pprint.pprint(keys[0]['Key'])
-    obj =s3.get_object(Key=keys[0]['Key'])
-    pprint.pprint(obj)
+    pprint.pprint(s3.get_object(keys[0]))
 
-    # client.get_object()
+    for k in keys:
+        pprint.pprint(k)
+        obj = s3.get_object(k)
+        if not collection.find_one({'slug': obj['slug']}):
+            collection.insert_one(obj)
+            pprint.pprint("added {}".format(k))
+        else:
+            pprint.pprint("key {} already exist".format(k))
---
+++ uboto3.py
@@ -0,0 +1,43 @@
+import boto3
+import os
+import json
+
+
+class UBoto3():
+
+    region_name = os.getenv('DO_SPACES_REGION')
+    endpoint_url = 'https://{}.digitaloceanspaces.com'.format(os.getenv('DO_SPACES_REGION'))
+    aws_access_key_id = os.getenv('DO_SPACES_KEY')
+    aws_secret_access_key = os.getenv('DO_SPACES_SECRET')
+    bucket = os.getenv('DO_BUCKET')
+
+    def __init__(self):
+
+        self.client = boto3.client('s3',
+                              region_name=self.region_name,
+                              endpoint_url=self.endpoint_url,
+                              aws_access_key_id=self.aws_access_key_id,
+                              aws_secret_access_key=self.aws_secret_access_key)
+
+    def list_objects(self, **kwargs):
+        """List of object found in the bucket
+
+        Parameters
+        ----------
+        Prefix: str
+            Prefix indicating the subfolder (slash is omited)
+        """
+        r = self.client.list_objects(Bucket=self.bucket, **kwargs)
+        return r.get('Contents')
+
+    def list_keys(self, **kwargs):
+        """Return list of keys of the found objects"""
+        rv = [k['Key'] for k in self.list_objects(**kwargs)]
+        return rv
+
+    def get_object(self, Key, **kwargs):
+        """Get a single object from the bucket using Key"""
+
+        r = self.client.get_object(Bucket=self.bucket, Key=Key, **kwargs)
+
+        return json.loads(r['Body'].read().decode('utf-8'))
