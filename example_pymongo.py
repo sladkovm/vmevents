4157e635a7eef4f5250c53813de69f92e8211f4f
sladkovm <sladkovm@gmail.com>
2018-09-22 15:12:25 +0200

    insert document

---
+++ example_pymongo.py
@@ -0,0 +1,37 @@
+import pymongo
+from pymongo import MongoClient
+import os
+from urllib.parse import quote_plus
+
+uri = "mongodb://{}:{}@{}".format(
+    os.getenv("ME_CONFIG_MONGODB_ADMINUSERNAME"),
+    os.getenv("ME_CONFIG_MONGODB_ADMINPASSWORD"),
+    os.getenv("MONGODB_URL"))
+
+client = MongoClient(uri)
+
+
+if client.address:
+    print(client.database_names())
+else:
+    print("Can not connect to MongoDB")
+
+"""
+An important note about collections (and databases) in MongoDB is
+that they are created lazily - none of the above commands
+have actually performed any operations on the MongoDB server.
+
+Collections and databases are created when the first document is inserted into them.
+"""
+
+db = client.test_database
+print(db)
+
+collection = db.test_collections
+print(collection)
+
+test_item = {"name": "Maksym",
+             "sex": "male"}
+
+item_id = collection.insert_one(test_item).inserted_id
+print(item_id)
