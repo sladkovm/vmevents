efb0349d7caaf001bdba8e7bd9b68eb01b5f110f
sladkovm <sladkovm@gmail.com>
2019-06-20 16:16:22 +0200

    /events/ is implemented and reads files from assets

--- api.py
+++ api.py
@@ -1,4 +1,6 @@
 import responder
+import os
+import json
 from loguru import logger
 
 api = responder.API()
@@ -7,6 +9,20 @@ api = responder.API()
 def greet_world(req, resp):
     logger.debug('vmevents')
     resp.text = "event api"
+
+
+@api.route("/events/")
+def events(req, resp):
+    """return all events as a json list"""
+    path = os.path.normpath("assets/events")
+    files = os.listdir(path)
+    logger.debug(files)
+    rv = []
+    for f_name in files:
+        with open(os.path.join(path, f_name)) as f:
+            _ = json.load(f)
+            rv.append(_)
+    resp.media = rv
 
 
 if __name__ == '__main__':
