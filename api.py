9dea4d06aa10c9e02b81b9a263fc0f16088a608f
sladkovm <sladkovm@gmail.com>
2019-06-22 12:50:43 +0200

    added results endpoint

--- api.py
+++ api.py
@@ -25,5 +25,28 @@ def events(req, resp):
     resp.media = rv
 
 
+@api.route("/events/{slug}")
+def event(req, resp, *, slug):
+    """return an event by it's slug"""
+    path = os.path.normpath("assets/events")
+    f_name = os.path.join(path, f"{slug}.json")
+    logger.debug(f_name)
+    with open(f_name) as f:
+        rv = json.load(f)
+    resp.media = rv
+
+
+@api.route("/results/{slug}")
+def results(req, resp, *, slug):
+    """return an event results by it's slug"""
+    path = os.path.normpath("assets/results")
+    f_name = os.path.join(path, f"{slug}.json")
+    logger.debug(f_name)
+    with open(f_name) as f:
+        rv = json.load(f)
+    resp.media = rv
+
+
+
 if __name__ == '__main__':
     api.run(address="0.0.0.0")
--- test_api.py
+++ test_api.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 import api as service
 
 
@@ -10,3 +11,27 @@ def api():
 def test_hello(api):
     r = api.requests.get("http://vmevents/hello")
     assert r.text == "hello"
+
+
+def test_events(api):
+    r = api.requests.get("http://vmevents/events/")
+    assert r.text is not None
+    events = json.loads(r.text)
+    assert events is not None
+    assert isinstance(events, list)
+
+
+def test_events_slug(api):
+    r = api.requests.get("http://vmevents/events/lbl-cyclo-2017")
+    assert r.text is not None
+    event = json.loads(r.text)
+    assert event is not None
+    assert isinstance(event, dict)
+
+
+def test_results_slug(api):
+    r = api.requests.get("http://vmevents/results/lbl-cyclo-2017")
+    assert r.text is not None
+    results = json.loads(r.text)
+    assert results is not None
+    assert isinstance(results, list)
